a145fd34eb9519503c8b915c467c042ee5fa2ee1
mimigargon <mireiag77@gmail.com>
2022-06-08 12:23:07 +0200

    First commit

---
+++ src/CV/CV.js
@@ -0,0 +1,84 @@
+export const CV ={
+    hero: {
+        name: 'Mireia',
+        lastname: 'García González',
+        city: 'Sabadell, Barcelona',
+        email: 'mireiag77@gmail.com',
+        birthdate: '21/03/1998',
+        phone: '600600600',
+        image: '',
+        gitHub: '',
+        aboutMe: [
+            {
+                info: ''
+            },
+            {
+                info: ''
+            },
+            {
+                info: ''
+            }, 
+        ],
+    },
+    education: [
+        {
+            name: 'Full Stack Developer Bootcamp',
+            date: '2022',
+            where: 'Upgrade Hub',
+        },
+        {
+            name: 'Master in Analysis and Management of Artistic Heritage',
+            date: '2020-2021',
+            where: 'Autonomous Univeristy of Barcelona',
+            internship: 'National Art Museum of Catalonia: Museography Department',
+        },
+        {
+            name: 'University Degree in Humanities',
+            date: '2016-2020',
+            where: 'Autonomous University of Barcelona',
+            internship: 'Badalona Local Museum: Documentation and Inventory Department',
+        },
+    ],
+    experience: [
+        {
+            name: 'Cleaning Worker',
+            date: '19/08/2021 - 19/07/2022',
+            where: 'Tot-Net',
+        },
+        {
+            name: 'Exhibtions Curator',
+            date: '03/2021 - 05/2022',
+            where: 'Autonomous University of Barcelona and Cerdanyola Art Museum',
+        },
+    ],
+    languages: [
+        {
+            language: 'Catalan',
+            wrlevel: 'Native',
+            splevel: 'Native',
+        },
+        {
+            language: 'Spanish',
+            wrlevel: 'Native',
+            splevel: 'Native', 
+        },
+        {
+            language: 'English',
+            wrlevel: 'C1',
+            splevel: 'C1', 
+        }, 
+    ],
+    habilites: [
+        'HTML',
+        'CSS/SASS',
+        'JavaScript',
+        'Node',
+        'React',
+        'SQL',
+        'Team work',
+        'Communication',
+        'Programming', 
+        'Telling stories',
+        'Good team environment',
+    ],
+};
